0bd8535dcff24fd9f632d598e581b6b85c15a473
Dylan_s <yangshao2014@gmail.com>
2018-09-12 22:45:34 -0700

    Update hook.py

--- hook.py
+++ hook.py
@@ -1,16 +1,15 @@
-import io
 import os
-from notebook.utils import to_api_path
+import nbformat
+from nbconvert.writers import FilesWriter
 
 _md_exporter = None
 
 def script_post_save(model, os_path, contents_manager, **kwargs):
     """convert notebooks to Python script after save with nbconvert
-
     replaces `ipython notebook --script`
     """
-    # from nbconvert.exporters.script import ScriptExporter
     from nbconvert.exporters.markdown import MarkdownExporter
+    log = contents_manager.log
 
     if model['type'] != 'notebook':
         return
@@ -20,13 +19,13 @@ def script_post_save(model, os_path, contents_manager, **kwargs):
     if _md_exporter is None:
         _md_exporter = MarkdownExporter(parent=contents_manager)
 
-    log = contents_manager.log
+    nb_node = nbformat.read(model['path'], nbformat.NO_CONVERT)
+    (output, resources) = _md_exporter.from_notebook_node(nb_node)
 
-    script, resources = _md_exporter.from_filename(os_path)
-    md_fname = os.path.dirname(os_path) + '/README' + resources.get('output_extension', '.md')
-    log.info("Saving README.md /%s", to_api_path(md_fname, contents_manager.root_dir))
+    c.FilesWriter.build_directory = os.path.dirname(os_path)
+    fw = FilesWriter(config=c)
+    fw.write(output, resources, notebook_name="README")
 
-    with io.open(md_fname, 'w', encoding='utf-8') as f:
-        f.write(script)
+    log.info("Saving README.md & dependencies files%s", os.path.dirname(os_path))
 
 c.FileContentsManager.post_save_hook = script_post_save
